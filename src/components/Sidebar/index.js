8375bbcb83abe050facb856f083d688a3110a772
Anthony Dube <dubedeveloper@gmail.com>
2022-04-07 19:46:28 -0400

    added sidebar

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,70 @@
+import { Link, NavLink } from 'react-router-dom'
+import './index.scss'
+import LogoAJD from '../../assets/images/logo-ajd.png'
+import LogoSignature from '../../assets/images/logo-sig.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import {
+  faLinkedin,
+  faGithub,
+  faDribbble,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons'
+
+const Sidebar = () => (
+  <div className="nav-bar">
+    <Link className="logo" to="/">
+      <img src={LogoAJD} alt="logo" />
+      <img className="sig-logo" src={LogoSignature} alt="Anthony J. Dube" />
+    </Link>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="about-link"
+        to="/about"
+      >
+        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="contact-link"
+        to="/contact"
+      >
+        <FontAwesomeIcon icon={faEnvelope} color="rgb(77,77,78)" />
+      </NavLink>
+    </nav>
+    <ul>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/ajdube/"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a target="_blank" rel="noreferrer" href="https://github.com/ajddev">
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a target="_blank" rel="noreferrer" href="https://dribbble.com/ajdube">
+          <FontAwesomeIcon icon={faDribbble} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a target="_blank" rel="noreferrer" href="https://twitter.com/ajddev">
+          <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
+        </a>
+      </li>
+    </ul>
+  </div>
+)
+
+export default Sidebar
