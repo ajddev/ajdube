16634a1b6b6d184dbed22503c340768b0d4da5fa
Anthony Dube <dubedeveloper@gmail.com>
2022-04-15 09:19:55 -0400

    refactored sidebar, created navigation/social components

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,20 +1,11 @@
-import { Link, NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './index.scss'
 import LogoAJD from '../../assets/images/logo-ajd.png'
 import LogoSignature from '../../assets/images/logo-sig.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-  faHome,
-  faUser,
-  faEnvelope,
-  faBars,
-} from '@fortawesome/free-solid-svg-icons'
-import {
-  faLinkedin,
-  faGithub,
-  faDribbble,
-  faTwitter,
-} from '@fortawesome/free-brands-svg-icons'
+import Navigation from '../Navigation'
+import Socials from '../Socials'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => (
   <>
@@ -26,57 +17,8 @@ const Sidebar = () => (
       <div className="menu">
         <FontAwesomeIcon icon={faBars} color="#fff" />
       </div>
-      <nav>
-        <NavLink exact="true" activeclassname="active" to="/">
-          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-        </NavLink>
-        <NavLink
-          exact="true"
-          activeclassname="active"
-          className="about-link"
-          to="/about"
-        >
-          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-        </NavLink>
-        <NavLink
-          exact="true"
-          activeclassname="active"
-          className="contact-link"
-          to="/contact"
-        >
-          <FontAwesomeIcon icon={faEnvelope} color="rgb(77,77,78)" />
-        </NavLink>
-      </nav>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://www.linkedin.com/in/ajdube/"
-          >
-            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
-          </a>
-        </li>
-        <li>
-          <a target="_blank" rel="noreferrer" href="https://github.com/ajddev">
-            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://dribbble.com/ajdube"
-          >
-            <FontAwesomeIcon icon={faDribbble} color="#4d4d4e" />
-          </a>
-        </li>
-        <li>
-          <a target="_blank" rel="noreferrer" href="https://twitter.com/ajddev">
-            <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
-          </a>
-        </li>
-      </ul>
+      <Navigation />
+      <Socials />
     </div>
   </>
 )
---
+++ src/components/Socials/index.js
@@ -0,0 +1,41 @@
+import './index.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faLinkedin,
+  faGithub,
+  faDribbble,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons'
+
+const Socials = () => {
+  return (
+    <ul>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/ajdube/"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a target="_blank" rel="noreferrer" href="https://github.com/ajddev">
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a target="_blank" rel="noreferrer" href="https://dribbble.com/ajdube">
+          <FontAwesomeIcon icon={faDribbble} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a target="_blank" rel="noreferrer" href="https://twitter.com/ajddev">
+          <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
+        </a>
+      </li>
+    </ul>
+  )
+}
+
+export default Socials
