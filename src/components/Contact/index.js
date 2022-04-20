0c37d7269a23626cb76fcac58d6d58dbc22d3758
Anthony Dube <dubedeveloper@gmail.com>
2022-04-20 09:56:32 -0400

    layout updates

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -29,31 +29,38 @@ const Contact = () => {
           <h2>
             Let's build something great together! I'd love to hear from you.
           </h2>
-          <form
-            action="https://formsubmit.co/dubedeveloper@gmail.com"
-            method="POST"
-          >
-            <div className="form-container">
-              <div className="name-email">
-                <input type="text" name="name" placeholder="Name" required />
-                <input type="email" name="email" placeholder="Email" required />
+          <div className="form-wrap">
+            <form
+              action="https://formsubmit.co/dubedeveloper@gmail.com"
+              method="POST"
+            >
+              <div className="form-container">
+                <div className="name-email">
+                  <input type="text" name="name" placeholder="Name" required />
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                </div>
               </div>
-            </div>
-            <div className="form-container">
-              <div className="name-email">
-                <textarea
-                  placeholder="Message"
-                  name="message"
-                  required
-                ></textarea>
+              <div className="form-container">
+                <div className="name-email">
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </div>
               </div>
-            </div>
-            <div className="form-container">
-              <button className="flat-button" type="submit">
-                SEND
-              </button>
-            </div>
-          </form>
+              <div className="form-container">
+                <button className="flat-button" type="submit">
+                  SEND
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </>
