1004a0f1d403b033f5d6a01aafa3f3643ef79aa7
Anthony Dube <dubedeveloper@gmail.com>
2022-04-20 10:36:37 -0400

    desktop style update

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -27,21 +27,25 @@ const About = () => {
               idx={15}
             />
           </h1>
-          <h2>
-            I'm a junior developer looking for a role in an established software
-            company where I can leverage and enhance my analytical, technical,
-            and computer skills.
-          </h2>
-          <h2>
-            I am quietly confident, naturally curious, and perpetually working
-            to improve my technique and knowledge. I'm a student of Udemy to
-            keep up-to-date with modern design.
-          </h2>
-          <h2>
-            I'm happily married and settled in Northern Vermont. My wife and I
-            recently adopted a rescue puppy. I enjoy traveling, playing chess
-            and listening to 90's rock.
-          </h2>
+          <div className="about-container">
+            <div className="about-wrapper">
+              <h2>
+                I'm a junior developer looking for a role in an established
+                software company where I can leverage and enhance my analytical,
+                technical, and computer skills.
+              </h2>
+              <h2>
+                I am quietly confident, naturally curious, and perpetually
+                working to improve my technique and knowledge. I'm a student of
+                Udemy to keep up-to-date with modern design.
+              </h2>
+              <h2>
+                I'm happily married and settled in Northern Vermont. My wife and
+                I recently adopted a rescue puppy. I enjoy traveling, playing
+                chess and listening to 90's rock.
+              </h2>
+            </div>
+          </div>
         </div>
         <div className="image-wrapper">
           <img className="pic" src={Me} />
